4ea0b34811623903106d7fa8411dadb94c9e6a2c
vitd39jc <tran.dinh.vi@jobchat.vn>
2020-08-14 09:15:52 +0700

    Config environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,26 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { CatsModule } from './cats/cats.module';
 import { EasyconfigModule } from 'nestjs-easyconfig';
-// import { DateScalar } from './common/scalars/date.scalar';
 import { MongooseModule } from '@nestjs/mongoose';
+// import { DateScalar } from './common/scalars/date.scalar';
+
+import { ConfigService } from './configs/config.service';
+import { ConfigModule } from './configs/config.module';
 
 @Module({
   // providers: [DateScalar],
   imports: [
     CatsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nestjsDemo'),
     EasyconfigModule.register({ path: '.env', safe: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('DB_AUTH')}${configService.get(
+          'DB_HOST',
+        )}:${configService.get('DB_PORT')}/${configService.get('DB_NAME')}`,
+      }),
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       installSubscriptionHandlers: true,
---
+++ src/configs/config.service.ts
@@ -0,0 +1,11 @@
+export class ConfigService {
+  private readonly envConfig: { [key: string]: string };
+
+  constructor() {
+    this.envConfig = process.env;
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+}
