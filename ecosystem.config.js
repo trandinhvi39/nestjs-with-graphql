f7011da76f3879017f08f44db75308d72ef65989
vitd39jc <tran.dinh.vi@jobchat.vn>
2020-08-21 14:43:38 +0700

    Config ecosystem and cors

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nestjs',
+      script: 'dist/main.js',
+      instances: 'max',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import rateLimit from 'express-rate-limit';
+import cors from 'cors';
 
 import { AppModule } from './app.module';
 import { ConfigService } from './configs/config.service';
@@ -15,9 +16,15 @@ async function bootstrap() {
       max: constant.rateLimit.max,
     }),
   );
-  app.enableCors();
   // see https://expressjs.com/en/guide/behind-proxies.html
   app.set('trust proxy', 1);
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization', 'Accept-Language'],
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get('GRAPHQL_SERVER_PORT'));
